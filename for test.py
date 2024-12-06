1fad26650d67a773c87b92b03f3dc7ed3513365a
Bangaart <planck2909@gmail.com>
2024-12-06 16:07:03 +0200

    initial commit

---
+++ for test.py
@@ -0,0 +1,14 @@
+def fib(n):
+    a,b,res= 10, 20 , 0
+    if n == 1:
+        print(a)
+    if n == 2:
+        print(b)
+    for i in range(n-2):
+       res = a+b
+       a,b = b, res
+    print(res)
+
+fib(6)
+
+
